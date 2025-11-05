0c5b217c432942518f88267819657964de17143a
José Tomás Lledó <jtlledo@uc.cl>
2025-11-05 19:37:18 -0300

    refactor: update Vite configuration to include allowed hosts for server

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  server: {
+    port: 5173,
+
+    allowedHosts: [
+      "slacc-frontend-1vz6.onrender.com",
+      "localhost",
+      "127.0.0.1",
+    ],
+  },
 });
-
-
